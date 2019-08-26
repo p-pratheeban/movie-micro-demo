300104dda51b997696840d22118983de888566b2
p-pratheeban <pratheeban.pancha@gmail.com>
2019-08-26 00:52:28 -0400

    Adding Fault Tolerance and Resilience Implementation

--- movie-catalog-service/src/main/java/com/pratheeban/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/pratheeban/resources/CatalogResource.java
@@ -7,39 +7,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.pratheeban.models.CatalogItem;
-import com.pratheeban.models.Movie;
 import com.pratheeban.models.UserRating;
+import com.pratheeban.services.MovieInfo;
+import com.pratheeban.services.UserRatingInfo;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
 
 	@Autowired
-	private RestTemplate restTemplate;
-	
+	private MovieInfo movieInfo;
+
 	@Autowired
-    WebClient.Builder webClientBuilder;
+	private UserRatingInfo userRatingInfo;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId,
-				UserRating.class);
+		UserRating userRating = userRatingInfo.getUserRating(userId);
 
 		return userRating.getRatings().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(),
-					Movie.class);
-			return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+			return movieInfo.getCatalog(rating);
 		}).collect(Collectors.toList());
 
 	}
-	
+
 }
 /*
-Alternative WebClient way
-Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
-.retrieve().bodyToMono(Movie.class).block();
-*/
+ * @Autowired WebClient.Builder webClientBuilder;
+ * 
+ * Alternative WebClient way Movie movie =
+ * webClientBuilder.build().get().uri("http://localhost:8082/movies/"+
+ * rating.getMovieId()) .retrieve().bodyToMono(Movie.class).block();
+ */
---
+++ movie-catalog-service/src/main/java/com/pratheeban/services/MovieInfo.java
@@ -0,0 +1,34 @@
+package com.pratheeban.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.pratheeban.models.CatalogItem;
+import com.pratheeban.models.Movie;
+import com.pratheeban.models.Rating;
+
+@Service
+public class MovieInfo {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+
+	})
+	public CatalogItem getCatalog(Rating rating) {
+		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+		return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+	}
+
+	private CatalogItem getFallbackCatalogItem(Rating rating) {
+		return new CatalogItem("Movie name not found", "", rating.getRating());
+	}
+}
